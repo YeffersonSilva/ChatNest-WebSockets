1718962255a3efa975028994afc20d29e8ce001a
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-05-20 16:03:59 -0300

    feat: add renderMessage function for displaying messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,11 +1,8 @@
-
-
-
 const username = localStorage.getItem('name');
 
-if(!username) {
-    window.location.href = '/';
-    throw new Error('Username is not set');
+if (!username) {
+  window.location.href = '/';
+  throw new Error('Username is not set');
 }
 
 const lblStatusOnline = document.querySelector('#status-online');
@@ -13,37 +10,70 @@ const lblStatusOffline = document.querySelector('#status-offline');
 
 const userUlElement = document.querySelector('ul');
 
+const input = document.querySelector('input');
+const form = document.querySelector('form');
+const chatElement = document.querySelector('#chat');
+
 const renderUsers = (users) => {
-    userUlElement.innerHTML = '';
-    users.forEach((user) => {
-        const li = document.createElement('li');
-        li.innerText = user.name;
-        userUlElement.appendChild(li);
-    })
+  userUlElement.innerHTML = '';
+  users.forEach((user) => {
+    const li = document.createElement('li');
+    li.innerText = user.name;
+    userUlElement.appendChild(li);
+  });
+};
 
-}
-
-const socket = io({
-    auth: {
-      token: 'ABC-123',
-        name :username
+const renderMessage = (payload) => {
+    const { userID, message, userName } = payload;
+    
+    const divElement = document.createElement('div');
+    divElement.classList.add('message');
+  
+    if (userID !== socket.id) {
+        divElement.classList.add('incoming');
+        
     }
+    
+    divElement.innerHTML = message;
+    chatElement.appendChild(divElement);
+
+    chatElement.scrollTop = chatElement.scrollHeight;
+
+};
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const message = input.value;
+    input
+
+    socket.emit('send-message', message);
 });
 
-
+const socket = io({
+  auth: {
+    token: 'ABC-123',
+    name: username,
+  },
+});
 
 socket.on('connect', () => {
-    lblStatusOnline.classList.remove('hidden');
-    lblStatusOffline.classList.add('hidden');
-})
+  lblStatusOnline.classList.remove('hidden');
+  lblStatusOffline.classList.add('hidden');
+});
 socket.on('disconnect', () => {
-    lblStatusOnline.classList.add('hidden');
-    lblStatusOffline.classList.remove('hidden');
-})
+  lblStatusOnline.classList.add('hidden');
+  lblStatusOffline.classList.remove('hidden');
+});
 
 socket.on('welcome-message', (data) => {
-    console.log(data);
-})
+  console.log(data);
+});
 
 socket.on('on-clients-changed', renderUsers);
-   
+
+
+socket.on('on-message', (data) => {
+    console.log(data);
+
+})
